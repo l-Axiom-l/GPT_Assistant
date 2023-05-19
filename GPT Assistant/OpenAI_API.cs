2fcb9b84aaacafe887fa76efa28e724ccfa56dcf
Axiom <axiom@mail.gmx>
2023-05-19 03:25:49 +0200

    Add project files.

---
+++ GPT Assistant/OpenAI_API.cs
@@ -0,0 +1,46 @@
+﻿using GPT_Assistant;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using static GPT_Assistant.ResponseModels;
+using Message = GPT_Assistant.Message;
+
+namespace OpenAI
+{
+    class OpenAI_API
+    {
+        string apikey = "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";
+
+        public async Task<string> Ask(string prompt)
+        {
+            string uri = "https://api.openai.com/v1/chat/completions";
+            HttpResponseMessage response;
+
+            using (HttpClient client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", apikey);
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                Message mes = new Message() {role = "user", content = prompt };
+                RequestModel request = new RequestModel()
+                {
+                    model = "gpt-3.5-turbo",
+                    messages = new Message[] {mes}
+                };
+                HttpContent content = new StringContent(JsonSerializer.Serialize(request), Encoding.UTF8, "application/json");
+                response = await client.PostAsync(uri, content);
+            };
+
+            GPTResponseModel model = (GPTResponseModel)await JsonSerializer.DeserializeAsync(response.Content.ReadAsStream(), typeof(GPTResponseModel));
+            return model.choices[0].message.content; 
+        }
+
+    }
+}
---
+++ GPT Assistant/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Net.Http.Headers;
+using System.Speech;
+using System.Speech.Recognition;
+using System.Speech.Synthesis;
+using OpenAI;
+
+
+using (SpeechRecognitionEngine rec = new SpeechRecognitionEngine(System.Globalization.CultureInfo.CurrentUICulture))
+{
+    Console.WriteLine("Active");
+    rec.LoadGrammar(new DictationGrammar());
+    rec.SpeechRecognized += new EventHandler<SpeechRecognizedEventArgs>(rec_SpeechRecognized);
+    rec.SetInputToDefaultAudioDevice();
+    rec.RecognizeAsync(RecognizeMode.Multiple);
+
+    // Keep the console window open.  
+    while (true)
+    {
+        Console.ReadLine();
+    }
+    Console.WriteLine("Inactive");
+};
+
+static void rec_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+{
+    Console.WriteLine("Recognized text: " + e.Result.Text);
+}
+
+//OpenAI_API api = new OpenAI_API();
+//string temp = await api.Ask(Console.ReadLine());
+
+
+SpeechSynthesizer synth = new SpeechSynthesizer();
+synth.SelectVoiceByHints(VoiceGender.Female, VoiceAge.NotSet, 0, System.Globalization.CultureInfo.CurrentUICulture);
+Prompt p = synth.SpeakAsync("");
+
+while(!p.IsCompleted)
+    await Task.Delay(100);
+
